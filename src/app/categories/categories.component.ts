c7d237c6715e8d1a12c828b0e58a3743f47da066
niemonie <s11914@pjwstk.edu.pl>
2017-04-10 20:53:07 +0200

    cleanup

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -3,8 +3,7 @@ import {Category} from "../shared/category";
 
 @Component({
   selector: 'app-categories',
-  templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.css']
+  templateUrl: './categories.component.html'
 })
 export class CategoriesComponent implements OnInit {
   selectedCategory: Category;
--- src/app/categories/category-item.component.ts
+++ src/app/categories/category-item.component.ts
@@ -1,16 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {Category} from "../shared/category";
 
-
 @Component({
   selector: 'app-category-item',
   templateUrl: './category-item.component.html',
-  styleUrls: ['./category-item.component.css']
 })
 export class CategoryItemComponent  {
 @Input() category: Category;
 @Input() categoryId: number;
-
 
 constructor(){};
 
