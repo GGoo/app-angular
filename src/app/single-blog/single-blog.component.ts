ff76bc632c1db632280679840b87a98ac3bfb3c1
GGoo <piekarska@live.com>
2017-04-30 21:51:35 +0200

    blog section

---
+++ src/app/single-blog/single-blog.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-blog',
+  templateUrl: './single-blog.component.html'
+})
+export class SingleBlogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
