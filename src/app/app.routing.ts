3689c307e8d72a3437de1995ec194e5551bc3683
GGoo <piekarska@live.com>
2017-06-20 23:18:51 +0200

    adding login&reg

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,13 +5,20 @@ import { PlannerCalendarComponent } from './plannercalendar/plannercalendar.comp
 import { DragndropComponent } from './dragndrop/dragndrop.component';
 import { BasketComponent } from './basket/basket.component';
 import { BlogComponent } from './blog/blog/blog.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RegisterPersonalComponent } from './register-personal/register-personal.component';
+import { RegisterCompanyComponent } from './register-company/register-company.component';
 
 import { CATEGORIES_ROUTES } from './categories.routes';
-
 
 const APP_ROUTES: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomepageComponent },
+  { path: 'logowanie', component: LoginComponent, pathMatch: 'full' },
+  { path: 'rejestracja', component: RegisterComponent, pathMatch: 'full' },
+  { path: 'rejestracja/konto-osobiste', component: RegisterPersonalComponent, pathMatch: 'full' },
+  { path: 'rejestracja/konto-firmowe', component: RegisterCompanyComponent, pathMatch: 'full' },
   { path: 'categories', component: CategoriesComponent, children: CATEGORIES_ROUTES },
   { path: 'calendar', component: PlannerCalendarComponent },
   { path: 'dragndrop', component: DragndropComponent },
---
+++ src/app/register-company/register-company.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-company',
+  templateUrl: './register-company.component.html'
+})
+export class RegisterCompanyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
